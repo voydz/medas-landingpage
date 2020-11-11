0532e35308fb22b88b16585df61f5c7cb2502f48
Felix Rudat <frudat@doegel.de>
2020-11-11 21:50:19 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,49 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>KVKDash</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
+        <div className={styles.logo}>
+          <Image
+            src="/logo/512px.png"
+            alt="KVKDash"
+            width={150}
+            height={150}
+          />
+        </div>
+
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          KVKDash
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Verteilerportal für Android und iOS
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/apps/current.ipa" className={styles.card}>
+            <h3>iOS &rarr;</h3>
+            <p>Lade KVKDash für dein iPhone oder iPad herunter.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="/apps/current.aab" className={styles.card}>
+            <h3>Android &rarr;</h3>
+            <p>Lade KVKDash für dein Android Telefon oder Tablet herunter.</p>
           </a>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <p className={styles.footnote}>
+          Derzeit ist KVKDash nur für Telefone und kleinere Bildschirme optimiert.
+        </p>
+      </main>
     </div>
   )
 }
