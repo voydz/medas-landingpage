552913fd28686b7a9736676914744a636cf8f758
Felix Rudat <frudat@doegel.de>
2020-12-21 15:22:41 +0100

    released KVKDash v1.2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
 import styles from '../styles/home.module.scss'
+import versions from '../versions.json'
+
+// get latest versions
+const ios = versions.ios[0];
+const android = versions.android[0];
 
 export default function Home() {
   return (
@@ -10,18 +15,23 @@ export default function Home() {
       </p>
 
       <div className={styles.grid}>
-        <a href="itms-services://?action=download-manifest&url=https://medas-landingpage.vercel.app/apps/manifest.plist" className={styles.card}>
+        <a href={ios.download} className={styles.card}>
           <h3>iOS &rarr;</h3>
           <span className={styles.muted}>Wenn dein Endgerät registriert ist, kannst du hier klicken, um KVKDash für dein iPhone oder iPad herunterzuladen.</span>
-          <p>Aktuelle Version: 1.0-r2</p>
-          <span>vom 11.11.2020 22:00 Uhr</span>
+          <p>
+            Aktuelle Version: {ios.version}
+            &nbsp;<span>(Build {ios.build})</span>
+          </p>
+          <span>vom {ios.date}</span>
         </a>
 
-        <a href="/apps/KVKDash.apk" className={styles.card}>
+        <a href={android.download} className={styles.card}>
           <h3>Android &rarr;</h3>
           <span className={styles.muted}>Hier klicken, um KVKDash für dein Android Telefon oder Tablet herunterzuladen.</span>
-          <p>Aktuelle Version: 1.0-r2</p>
-          <span>vom 11.11.2020 22:00 Uhr</span>
+          <p>Aktuelle Version: {android.version}
+            &nbsp;<span>(Build {android.build})</span>
+          </p>
+          <span>vom {android.date}</span>
         </a>
       </div>
 
